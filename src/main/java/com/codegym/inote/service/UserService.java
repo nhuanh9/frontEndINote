b3fbc6fcf57199deaaa7397d37affe545eba62a0
quan200699 <maplestory2012vn@gmail.com>
2019-08-20 16:06:27 +0700

    register

--- src/main/java/com/codegym/inote/service/UserService.java
+++ src/main/java/com/codegym/inote/service/UserService.java
@@ -5,4 +5,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UserService extends UserDetailsService {
     void save(User user);
+
+    Iterable<User> findAll();
 }
--- src/main/java/com/codegym/inote/service/impl/UserServiceImpl.java
+++ src/main/java/com/codegym/inote/service/impl/UserServiceImpl.java
@@ -14,6 +14,7 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+
     @Override
     public UserDetails loadUserByUsername(String username) {
         User user = userRepository.findByUsername(username);
@@ -27,5 +28,10 @@ public class UserServiceImpl implements UserService {
     @Override
     public void save(User user) {
         userRepository.save(user);
+    }
+
+    @Override
+    public Iterable<User> findAll() {
+        return userRepository.findAll();
     }
 }
