e7b156c43cba0d508c7aa850ffbb223d2273f88f
quan200699 <maplestory2012vn@gmail.com>
2019-08-23 13:43:14 +0700

    recycle bin

--- src/main/java/com/codegym/inote/controller/RecycleBinController.java
+++ src/main/java/com/codegym/inote/controller/RecycleBinController.java
@@ -6,8 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -23,6 +22,35 @@ public class RecycleBinController {
 
         ModelAndView modelAndView = new ModelAndView("/recycleBin/list");
         modelAndView.addObject("trashes", trashes);
+        return modelAndView;
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView showDeleteForm(@PathVariable Long id) {
+        Trash trash = recycleBinService.findById(id);
+        if (trash != null) {
+            ModelAndView modelAndView = new ModelAndView("/recycleBin/delete");
+            modelAndView.addObject("trash", trash);
+            return modelAndView;
+        }
+        return new ModelAndView("/error-404");
+    }
+
+    @PostMapping("/delete")
+    public String deleteNoteType(@ModelAttribute Trash trash) {
+        recycleBinService.remove(trash.getId());
+        return "redirect:/user/recycleBin/trashes";
+    }
+
+    @GetMapping("/view/{id}")
+    public ModelAndView viewNote(@PathVariable Long id) {
+        Trash trash = recycleBinService.findById(id);
+        if (trash == null) {
+            return new ModelAndView("/error-404");
+        }
+
+        ModelAndView modelAndView = new ModelAndView("/recycleBin/view");
+        modelAndView.addObject("trash", trash);
         return modelAndView;
     }
 }
